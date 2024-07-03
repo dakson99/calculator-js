7d7580dfec9726b2dc38d48ddc1f2f061619e3cf
dakson99 <darko99.mikavica@gmail.com>
2024-07-03 16:56:26 +0200

    Implement equals operation

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ const container = document.querySelector('.container');
 
 // functions
 const add = function (a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 };
 
 const subtract = function (a, b) {
-    return a - b;
+    return parseFloat(a) - parseFloat(b);
 };
 
 const multiply = function (a, b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 };
 
 const divide = function (a, b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);
 };
 
 function operate(a, b, op) {
-    if (op === '+') return add(a, b);
-    if (op === '*') return multiply(a, b);
-    if (op === '-') return subtract(a, b);
-    if (op === '/') return divide(a, b);
+    if (op == '+') return add(a, b);
+    if (op == '*') return multiply(a, b);
+    if (op == '-') return subtract(a, b);
+    if (op == '/') return divide(a, b);
 }
 
 let firstNum = '';
@@ -37,10 +37,13 @@ let operator = '';
 let stateTracker = false;
 let result;
 
+const btnEqual = document.querySelector('.btn__operation__equal');
+
 numbers.forEach((el) =>
     el.addEventListener('click', function (e) {
         if (outputMain.textContent === '0') outputMain.textContent = '';
         outputMain.textContent += el.textContent;
+
         firstNum = +outputMain.textContent;
     })
 );
@@ -53,7 +56,7 @@ operations.forEach((el) =>
             operator = e.target.dataset.operation;
             secondNum = result;
             outputMain.textContent = '0';
-            outputSecondary.textContent = secondNum;
+            outputSecondary.textContent = `${secondNum} ${operator}`;
             console.log(firstNum, secondNum, result);
         } else {
             operator = e.target.dataset.operation;
@@ -61,8 +64,15 @@ operations.forEach((el) =>
             secondNum = firstNum;
             firstNum = '';
             outputMain.textContent = '0';
-            outputSecondary.textContent = secondNum;
+            outputSecondary.textContent = `${secondNum} ${operator}`;
             console.log(firstNum, secondNum, result);
         }
     })
 );
+
+btnEqual.addEventListener('click', function (e) {
+    const resultEqual = operate(secondNum, firstNum, operator);
+    outputSecondary.textContent = `${resultEqual} ${operator}`;
+    outputMain.textContent = '0';
+    console.log(firstNum, secondNum);
+});
