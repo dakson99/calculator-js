4fb37724e9c55b54b863522600f4bb203d25d039
dakson99 <darko99.mikavica@gmail.com>
2024-07-10 13:51:08 +0200

    add comment

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // selectors
-
 const btnsContainer = document.querySelector('.buttons');
-
 const outputMain = document.querySelector('[data-output-main]');
 const outputSecondary = document.querySelector('[data-output-secondary]');
 const numbers = document.querySelectorAll('[data-number]');
@@ -9,24 +7,18 @@ const operations = document.querySelectorAll('[data-operation ]');
 const btnEqual = document.querySelector('[data-equals]');
 const btnDelete = document.querySelector('[data-delete]');
 const btnClear = document.querySelector('[data-all-clear]');
-
 class Calculator {
     firstNum;
     secondNum;
     operator;
-
     constructor(outputMain, outputSecondary) {
         this.outputMain = outputMain;
         this.outputSecondary = outputSecondary;
-
         this.clear();
     }
-
-
     appendNumber(num) {
         if (num === '.' && this.firstNum.toString().includes('.')) return;
         if (num === '.' && this.firstNum.toString().length === 0) return;
-
         this.firstNum = this.firstNum.toString() + num.toString();
     }
     chooseOperation(op) {
@@ -38,13 +30,10 @@ class Calculator {
         this.firstNum = '';
         this.operator = op;
     }
-
     operate() {
-
         let result;
         const first = Number(this.firstNum);
         const second = Number(this.secondNum);
-
         switch (this.operator) {
             case '+':
                 result = second + first;
@@ -71,18 +60,15 @@ class Calculator {
         this.secondNum = '';
         this.operation = '';
     }
-
     updateDisplay() {
         if (this.firstNum === '')
             this.outputSecondary.textContent = `${this.secondNum} ${this.operator}`;
         if (this.secondNum === '') this.outputSecondary.textContent = '';
         this.outputMain.textContent = this.firstNum;
     }
-
     deleteLastDigit() {
         this.firstNum = this.firstNum.slice(0, -1);
     }
-
     clear() {
         this.firstNum = '';
         this.secondNum = '';
@@ -92,30 +78,27 @@ class Calculator {
 
 const calculator = new Calculator(outputMain, outputSecondary);
 
+// event listeners
 numbers.forEach((num) =>
     num.addEventListener('click', function () {
         calculator.appendNumber(num.textContent);
         calculator.updateDisplay();
     })
 );
-
 operations.forEach((op) =>
     op.addEventListener('click', function () {
         calculator.chooseOperation(op.textContent);
         calculator.updateDisplay();
     })
 );
-
 btnEqual.addEventListener('click', function () {
     calculator.operate();
     calculator.updateDisplay();
 });
-
 btnDelete.addEventListener('click', function () {
     calculator.deleteLastDigit();
     calculator.updateDisplay();
 });
-
 btnClear.addEventListener('click', function () {
     calculator.clear();
     calculator.updateDisplay();
